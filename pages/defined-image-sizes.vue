0375285be4a62594f2ec76e84f28188e4bf1c6eb
Larry W. <l422y@l422y.com>
2024-01-03 09:39:00 -0500

    Add defined-image-sizes.vue with static image sizes and color generator

---
+++ pages/defined-image-sizes.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>With static/defined image sizes</h1>
+        <h2 v-for="n in 20" :style="{background: generators.colorForNumber(n)}">
+            {{ n }}
+            <img :height="n * 18" :src="generators.imageUrlForN(n)" :width="n * 20" alt="">
+        </h2>
+    </div>
+</template>
+<script lang="ts" setup>
+const generators = useGenerators()
+</script>
