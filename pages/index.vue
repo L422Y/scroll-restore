16f790e61f3bab78c7d2d6e4274f9c8a4b798061
Larry W. <l422y@l422y.com>
2024-01-03 09:39:04 -0500

    Add index.vue with basic content and dynamic color generator

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>With basic content only, no images</h1>
+        <h2 v-for="n in 100" :style="{background: generators.colorForNumber(n)}">
+            {{ n }}
+        </h2>
+    </div>
+</template>
+<script lang="ts" setup>
+const generators = useGenerators()
+</script>
