4e334a4151011c40731bc5cc3c518fb30a1c5500
Larry W. <l422y@l422y.com>
2024-01-03 08:50:16 -0500

    Add Nuxt 3 minimal starter project with basic configurations and dependencies

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1 v-for="n in 100" :style="{background: colorForNumber(n)}">
+            {{ n }}
+        </h1>
+    </div>
+</template>
+<script lang="ts" setup>
+const colorForNumber = (n: number) => {
+    const hue = n * 137.508 // use golden angle approximation
+    return `hsl(${hue},50%,75%)`
+}
+</script>
+<style scoped>
+h1 {
+    font-size: 10rem;
+    text-align: center;
+}
+</style>
