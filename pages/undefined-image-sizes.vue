486988c6c6d589cef54c54f09bbae4eb8c9229fc
Larry W. <l422y@l422y.com>
2024-01-03 09:39:08 -0500

    Add undefined-image-sizes.vue with static image sizes display

---
+++ pages/undefined-image-sizes.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>With static/defined image sizes</h1>
+        <h2 v-for="n in 20" :style="{background: generators.colorForNumber(n)}">
+            {{ n }}
+            <img :src="generators.imageUrlForN(n)" alt="">
+        </h2>
+    </div>
+</template>
+<script lang="ts" setup>
+const generators = useGenerators()
+</script>
