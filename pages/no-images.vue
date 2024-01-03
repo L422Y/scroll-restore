40ccf361e084e27cb55824cd1efdeec8b770cd8b
Larry W. <l422y@l422y.com>
2024-01-03 10:00:26 -0500

    Add no-images.vue page with basic HTML content and dynamic color generation

---
+++ pages/no-images.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Basic HTML Content Only/No Imges</h1>
+        <h2 v-for="n in 20" :style="{background: generators.colorForNumber(n)}">
+            {{ n }}
+        </h2>
+    </div>
+</template>
+<script lang="ts" setup>
+const generators = useGenerators()
+</script>
